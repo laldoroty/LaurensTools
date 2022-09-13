5097129449feae35db020d951578dd90aba0644e
astrolab <laurenaldoroty@gmail.com>
2022-09-13 17:56:23 -0500

    split synth_lc into flux and mag functions separately

--- LaurensTools/photometry/synthphot_combined.py
+++ LaurensTools/photometry/synthphot_combined.py
@@ -24,7 +24,7 @@ def load_filtersys(sys):
     """
     20220627 LNA
     Loads the transmission curves for the specified filter system.
-    sys -- filter system. Options are 'bessell', 'sdss', 'lsst', 'lsst_filteronly'.
+    sys -- filter system. Options are 'bessell', 'sdss', 'lsst', 'lsst_filteronly', 'snftophat'.
     Bessell response function from: https://ui.adsabs.harvard.edu/abs/2012PASP..124..140B/abstract Zero points from table 3. Compare errors to the errors in: https://arxiv.org/pdf/0910.3330.pdf 
     For SDSS, response function from: https://arxiv.org/abs/1002.3701 (http://www.ioa.s.u-tokyo.ac.jp/~doi/sdss/SDSSresponse.html)
     SDSS zeropoints are copied from SNooPy.
@@ -93,11 +93,23 @@ def load_filtersys(sys):
             responsefunc = pd.read_csv(os.path.join(dirname,f'filters/lsst_filters/filter_only/LSST_LSST.{band}_filter.dat'), sep=' ')
             filtersys[band] = {'wavelength': responsefunc[f'lam_{band}'], 'transmission': responsefunc[f'{band}']}
             zeropoints[band] = 2.5*np.log10(zeropoints[band])
+    elif sys == 'snftophat':
+        filter_edges = {'u': (3300., 4102.),
+            'b': (4102., 5100.),
+            'v': (5200., 6289.),
+            'r': (6289., 7607.),
+            'i': (7607., 9200.)}
+        filtersys = {}
+        zeropoints = {}
+        for fname, edges in filter_edges.items():
+            wave = [edges[0]-1., edges[0], edges[1], edges[1]+1.]
+            trans = [0., 1., 1., 0.]
+            filtersys[fname] = {'wavelength': wave, 'transmission': trans}
+            zeropoints[fname] = 0.
 
     return filtersys, zeropoints
 
-
-def synth_lc_tophat(wave,flux,var,lower_filter_edge=None,upper_filter_edge=None,standard='vega',spec_units='ergs'):
+def synth_lc_tophat(wave,flux,var,lower_filter_edge=None,upper_filter_edge=None,standard='vega',spec_units='ergs',verbose=False):
     """
     20210520 LNA
     Make a synthetic magnitude from a spectrum using a top hat filter.
@@ -122,10 +134,11 @@ def synth_lc_tophat(wave,flux,var,lower_filter_edge=None,upper_filter_edge=None,
 
         if spec_units in acceptable_spec_units:
             if spec_units == 'ergs':
-                print('Spectrum units ergs/cm^2/s/AA. Converting to photons.')
+                if verbose:
+                    print('Spectrum units ergs/cm^2/s/AA. Converting to photons.')
                 flux /= h*c/wave
                 st_flux /= h*c/st_wav
-            elif spec_units == 'photons':
+            elif spec_units == 'photons' and verbose:
                 print('Spectrum units not converted; already in photons.')
         else:
             raise ValueError(f'Acceptable spec_units arguments are {acceptable_spec_units}.')
@@ -145,11 +158,11 @@ def synth_lc_tophat(wave,flux,var,lower_filter_edge=None,upper_filter_edge=None,
     
     return mag, emag
 
-def synth_lc(wave,flux,var,sys=None,standard='vega',spec_units='ergs'):
+def band_flux(wave,flux,var,sys=None,standard='vega',spec_units='ergs',verbose=False):
     """
-    Make a synthetic magnitude from a spectrum using Bessell, SDSS, or LSST filters. 
+    Retrieve flux in all bands for Bessell, SDSS, SNfactory top hat, or LSST filters.
     Filters are provided in photon counts, so this code converts ergs to photons
-    for the input flux. 
+    for the input flux.
     Vega is the only available standard, currently. 
 
     Keyword arguments:
@@ -168,8 +181,8 @@ def synth_lc(wave,flux,var,sys=None,standard='vega',spec_units='ergs'):
 
     h = 6.626E-27 # Planck constant, erg s
     c = 3E18 # Speed of light, AA/s
-    photometry = {}
-    ephotometry = {}
+    F = {}
+    eF = {}
 
     if standard=='vega':
         st_wav, st_flux, st_flux_error = load_vegaspec()
@@ -178,15 +191,16 @@ def synth_lc(wave,flux,var,sys=None,standard='vega',spec_units='ergs'):
 
         if spec_units in acceptable_spec_units:
             if spec_units == 'ergs':
-                print('Spectrum units ergs/cm^2/s/AA. Converting to photons.')
+                if verbose:
+                    print('Spectrum units ergs/cm^2/s/AA. Converting to photons.')
                 flux /= h*c/wave
                 st_flux /= h*c/st_wav
-            elif spec_units == 'photons':
+            elif spec_units == 'photons' and verbose:
                 print('Spectrum units not converted; already in photons.')
         else:
             raise ValueError(f'Acceptable spec_units arguments are {acceptable_spec_units}.')
 
-        F = 0
+        F_ = 0
         Fref = 0
         var_F = 0
         dlam = np.diff(wave)
@@ -195,11 +209,41 @@ def synth_lc(wave,flux,var,sys=None,standard='vega',spec_units='ergs'):
         for band in filters:
             responsefunc_interp = interp1d(responsefunc[band]['wavelength'], responsefunc[band]['transmission'],kind='linear',bounds_error=False)
 
-            F = np.sum(flux[1:]*np.nan_to_num(responsefunc_interp(wave)[1:])*wave[1:]*dlam)
+            F_ = np.sum(flux[1:]*np.nan_to_num(responsefunc_interp(wave)[1:])*wave[1:]*dlam)
             var_F = np.sum(wave[1:]**2*var[1:]*np.nan_to_num(responsefunc_interp(wave)[1:])**2*dlam**2)
             Fref = np.sum(st_flux[1:]*np.nan_to_num(responsefunc_interp(st_wav)[1:])*st_wav[1:]*st_dlam)
 
-            photometry[band] = -2.5*np.log10(F/Fref)
-            ephotometry[band] = np.sqrt((1.09/F)**2*var_F)
+            F[band] = F_/Fref
+            eF[band] = np.sqrt(var_F)
+
+    return F, eF
+
+def synth_lc(wave,flux,var,sys=None,standard='vega',spec_units='ergs',verbose=False,return_flux=False):
+    """
+    Make a synthetic magnitude from a spectrum using Bessell, SDSS, SNfactory top hat, or LSST filters. 
+    Filters are provided in photon counts, so this code converts ergs to photons
+    for the input flux. 
+    Vega is the only available standard, currently. 
+
+    Keyword arguments:
+    wave -- Data spectrum wavelength
+    flux -- Data spectrum flux
+    var -- Data spectrum variance
+    sys -- Which filter system to use. Valid values are: ['bessell', 'sdss', 'lsst', 'lsst_filteronly'].
+    standard -- Which standard spectrum to use. Valid values are: ['vega'].
+    convert_spec -- 'ergs' or 'photons'. 'ergs' converts the input data spectrum to ergs. 'photons' converts it to photons. 
+    convert_response -- 'ergs' or 'photons'. 'ergs' converts the response function to ergs. 'photons' converts it to photons. 
+    """
+
+    flux, eflux = band_flux(wave,flux,var,sys=sys,standard=standard,spec_units=spec_units,verbose=verbose)
+    photometry = {}
+    ephotometry = {}
+
+    for band in flux.keys():
+        photometry[band] = -2.5*np.log10(flux[band])
+        ephotometry[band] = np.sqrt((1.09/flux[band])**2*eflux[band]**2)
                 
-    return photometry, ephotometry
+    if return_flux:
+        return photometry, ephotometry, flux, eflux
+    else:
+        return photometry, ephotometry
